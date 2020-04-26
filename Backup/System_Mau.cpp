b5d4f640ea02da9baf535ea0a8503e36f13ef88b
nhatcuongti <63848033+nhatcuongti@users.noreply.github.com>
2020-04-26 22:12:09 +0700

    Update again

---
+++ Backup/System_Mau.cpp
@@ -0,0 +1,34 @@
+#include "Header_Mau.h"
+
+//User Interface support Function//
+void clearScreen()
+{
+	system("CLS");
+}
+
+void warnScreen()
+{
+	cout << "Your choice isn't correct !! Please choose again" << endl;
+}
+
+int choiceScreen(int Number)
+{
+	string Choice;					
+	cout << "Enter your choice (0 -> " << Number << ") :" ; cin >> Choice;
+	return checkChoice(Choice, Number);
+}
+//*******************************//
+
+int checkChoice(string Choice, int Number)
+{
+	int length = Choice.length();
+	int numberCheck = 0;
+	for (int i = 0 ; i < length; i++)
+		if ((Choice[i] >= '0') && (Choice[i] <= '9'))
+			numberCheck += int(Choice[i]) - 48;
+		else 
+			return -1;		
+	if (numberCheck <= Number)						
+		return numberCheck;
+	return -1;
+}
---
+++ Backup/main_Mau.cpp
@@ -0,0 +1,8 @@
+#include "Header_Mau.h"
+
+int main()
+{
+	cout << "Welcome to my Student Management System" << endl;
+	firstUI();
+	return 0;
+}
